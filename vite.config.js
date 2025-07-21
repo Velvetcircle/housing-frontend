65febc8a5dddd30f52c5c0d5fb71272e1c956982
Velvetcircle <velvetcircleindia@gmail.com>
2025-07-21 19:37:34 +0530

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './', // ← VERY IMPORTANT for correct file paths in deployment
+})
